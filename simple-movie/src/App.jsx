4b84394485abdf7eb0f7e98a523bdbb30d3a884e
long.nguyen <long29103107@gmail.com>
2025-12-24 23:36:36 +0700

    update tailwindcss

--- simple-movie/src/App.jsx
+++ simple-movie/src/App.jsx
@@ -1,9 +1,26 @@
+import { NavLink } from "react-router-dom";
+
 const App = () => {
   return (
-    <h1 className="text-3xl text-red-500 bg-blue-500 text-center">
-      Hello world!
-    </h1>
+    <>
+      <header className="header flex items-center justify-center gap-x-5 text-white py-10 mb-10">
+        <span className="text-primary">Home</span>
+        <span>Movies</span>
+      </header>
+      <section className="banner h-[400px] page-container">
+        <div className="w-full h-full rounded-lg relative">
+          <img
+            src="https://i.pinimg.com/736x/39/10/6b/39106be8cfa75df021dc00bf46b8055b.jpg"
+            alt=""
+            className="w-full h-full object-cover rounded-lg"
+          />
+          <div className="absolute left-5 bottom-5 w-full text-white">
+            <h2 className="font-bold text-3xl mb-5">The Dark Knight</h2>
+          </div>
+        </div>
+      </section>
+    </>
   );
-}
+};
 
 export default App;
---
+++ simple-movie/tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,scss,css}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['DM Sans', 'sans-serif'],
+      },
+      colors: {
+        primary: '#F62682',
+        secondary: '#6F5CF1',
+      },
+    },
+  },
+  plugins: [],
+};
