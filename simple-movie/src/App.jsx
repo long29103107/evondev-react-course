fb4fe64a5e9a5cf08bd293a0d3e8a010c08496a8
blogic-hoanglongnguyen <hoanglongnguyen@blogiclab.com>
2025-12-25 16:22:50 +0700

    Lesson 198: Menu và Banner

--- simple-movie/src/App.jsx
+++ simple-movie/src/App.jsx
@@ -5,20 +5,26 @@ const App = () => {
     <>
       <header className="header flex items-center justify-center gap-x-5 py-10 mb-10">
           <span className="text-primary">Home</span>
-          <span className="text-secondary">Movies</span>
+          <span className="text-white">Movies</span>
       </header>
+
       <section className="banner h-[400px] rounded-lg bg-slate-800 page-container mb-10">
+      
         <div className="w-full h-full rounded-lg relative">
+      
+          <div className="overlay absolute inset-0 bg-gradient-to-t from-[rgba(0,0,0,0.5)] to-[rgba(0,0,0,0.5)] rounded-lg"></div>
+
           <img src="https://genk.mediacdn.vn/2018/10/13/photo-1-15394076422481094585130.jpg" 
-          alt="banner" className="w-full h-full object-cover rounded-lg" />
-          <div className="absolute left-5 bottom-0 w-full text-white">
-            <h2 className="text-3xl font-bold mb-5">End game</h2>
+            alt="banner" className="w-full h-full object-cover rounded-lg" />
+          
+          <div className="absolute left-5 bottom-5 w-full text-white">
+            <h2 className="text-3xl font-bold mb-5">Avengers: Endgame</h2>
             <div className="flex items-center gap-x-3 mb-8">
               <span className="py-2 px-4 border border-white rounded-md">Adventure</span>
               <span className="py-2 px-4 border border-white rounded-md">Action</span>
               <span className="py-2 px-4 border border-white rounded-md">Drama</span>
             </div>
-            <button className="py-3 px-6 rounded-lg bg-primary-500 text-white font-medium">Watch now</button>
+            <button className="py-3 px-6 rounded-lg bg-primary text-white font-medium">Watch now</button>
           </div>
         </div>
       </section>
