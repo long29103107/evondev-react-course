949d4ca07899b3687d9ea3f0fa8367fa069220e6
blogic-hoanglongnguyen <hoanglongnguyen@blogiclab.com>
2025-12-25 10:44:41 +0700

    update simple movie

--- simple-movie/src/App.jsx
+++ simple-movie/src/App.jsx
@@ -3,19 +3,22 @@ import { NavLink } from "react-router-dom";
 const App = () => {
   return (
     <>
-      <header className="header flex items-center justify-center gap-x-5 text-white py-10 mb-10">
-        <span className="text-primary font-body">Home</span>
-        <span>Movies</span>
+      <header className="header flex items-center justify-center gap-x-5 py-10 mb-10">
+          <span className="text-primary">Home</span>
+          <span className="text-secondary">Movies</span>
       </header>
-      <section className="banner h-[400px] page-container">
+      <section className="banner h-[400px] rounded-lg bg-slate-800 page-container mb-10">
         <div className="w-full h-full rounded-lg relative">
-          <img
-            src="https://i.pinimg.com/736x/39/10/6b/39106be8cfa75df021dc00bf46b8055b.jpg"
-            alt=""
-            className="w-full h-full object-cover rounded-lg"
-          />
-          <div className="absolute left-5 bottom-5 w-full text-white">
-            <h2 className="font-bold text-3xl mb-5">The Dark Knight</h2>
+          <img src="https://genk.mediacdn.vn/2018/10/13/photo-1-15394076422481094585130.jpg" 
+          alt="banner" className="w-full h-full object-cover rounded-lg" />
+          <div className="absolute left-5 bottom-0 w-full text-white">
+            <h2 className="text-3xl font-bold mb-5">End game</h2>
+            <div className="flex items-center gap-x-3 mb-8">
+              <span className="py-2 px-4 border border-white rounded-md">Adventure</span>
+              <span className="py-2 px-4 border border-white rounded-md">Action</span>
+              <span className="py-2 px-4 border border-white rounded-md">Drama</span>
+            </div>
+            <button className="py-3 px-6 rounded-lg bg-primary-500 text-white font-medium">Watch now</button>
           </div>
         </div>
       </section>
