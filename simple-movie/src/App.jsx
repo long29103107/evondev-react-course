c8f46a5945939143125d7ea9d10a6aa0bea9e78c
blogic-hoanglongnguyen <hoanglongnguyen@blogiclab.com>
2025-12-25 08:51:45 +0700

    update config

--- simple-movie/src/App.jsx
+++ simple-movie/src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <>
       <header className="header flex items-center justify-center gap-x-5 text-white py-10 mb-10">
-        <span className="text-primary">Home</span>
+        <span className="text-primary font-body">Home</span>
         <span>Movies</span>
       </header>
       <section className="banner h-[400px] page-container">
--- simple-movie/vite.config.js
+++ simple-movie/vite.config.js
@@ -2,13 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import tailwindcss from '@tailwindcss/vite'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'), 
     },
+  },
+  server: {
+    port: 3000, 
   },
 })
