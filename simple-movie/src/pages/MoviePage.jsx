4640e06201a2a8aef8902e8a3cdb6a50920363af
blogic-hoanglongnguyen <hoanglongnguyen@blogiclab.com>
2025-12-29 08:46:42 +0700

    add keydown search

--- simple-movie/src/pages/MoviePage.jsx
+++ simple-movie/src/pages/MoviePage.jsx
@@ -1,44 +1,47 @@
 import React from "react";
-import MovieList from "@/components/movie/MovieList";
 import useSWR from "swr";
 import { fetcher, apiKey } from "@/config";
 import MovieCard from "@/components/movie/MovieCard";
+import { useState, useRef } from "react";
 
 const MoviePage = () => {
-  const { data } = useSWR(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`,
-    fetcher
-  );
+  const [query, setQuery] = useState("");
+  const inputRef = useRef(null);
 
+  const url = query
+    ? `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`
+    : `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
+
+  const { data } = useSWR(url, fetcher);
   const movies = data?.results || [];
+
+  const handleSearch = () => {
+    setQuery(inputRef.current.value);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
 
   return (
     <div className="py-10 page-container">
       <div className="flex mb-10">
         <div className="flex-1">
           <input
+            ref={inputRef}
             type="text"
             placeholder="Type here to search..."
             className="w-full p-4 bg-transparent outline-none bg-slate-800 rounded-lg text-white"
+            onKeyDown={handleKeyDown}
           />
         </div>
-        <button className="p-4 bg-primary text-white">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-            />
-          </svg>
+        <button className="p-4 bg-primary text-white" onClick={handleSearch}>
+          🔍
         </button>
       </div>
+
       <div className="grid grid-cols-4 gap-10">
         {movies.map((movie) => (
           <MovieCard key={movie.id} item={movie} />
