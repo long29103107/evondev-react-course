261365619912bccea96a6ba69970bab1861b96b4
blogic-hoanglongnguyen <hoanglongnguyen@blogiclab.com>
2026-01-05 09:48:56 +0700

    done lesson 209

--- simple-movie/src/pages/MoviePage.jsx
+++ simple-movie/src/pages/MoviePage.jsx
@@ -2,39 +2,56 @@ import React from "react";
 import useSWR from "swr";
 import { fetcher, apiKey } from "@/config";
 import MovieCard from "@/components/movie/MovieCard";
-import { useState, useRef } from "react";
+import { useState, useMemo, useEffect } from "react";
+import useDebounce from "../hooks/useDebouce";
 
 const MoviePage = () => {
-  const [query, setQuery] = useState("");
-  const inputRef = useRef(null);
+  const [filter, setFilter] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+  const filterDebounce = useDebounce(filter, 500);
+  
+  useEffect(() => {
+    setSearchTerm(filterDebounce);
+  }, [filterDebounce]);
 
-  const url = query
-    ? `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`
-    : `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
+  const url = useMemo(() => {
+    if (!searchTerm) {
+      return `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
+    }
+  
+    return `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(
+      searchTerm
+    )}`;
+  }, [searchTerm]);
 
-  const { data } = useSWR(url, fetcher);
-  const movies = data?.results || [];
-
-  const handleSearch = () => {
-    setQuery(inputRef.current.value);
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
   };
 
-  const handleKeyDown = (e) => {
+  const handleSearch = () => {
+    if (!filter.trim()) return;
+    setSearchTerm(filter);
+  };
+
+  const keydown = (e) => {
     if (e.key === "Enter") {
       handleSearch();
     }
   };
+
+  const { data } = useSWR(url, fetcher);  
+  const movies = data?.results || [];
 
   return (
     <div className="py-10 page-container">
       <div className="flex mb-10">
         <div className="flex-1">
           <input
-            ref={inputRef}
             type="text"
             placeholder="Type here to search..."
             className="w-full p-4 bg-transparent outline-none bg-slate-800 rounded-lg text-white"
-            onKeyDown={handleKeyDown}
+            onChange={handleFilterChange}
+            onKeyDown={keydown}
           />
         </div>
         <button className="p-4 bg-primary text-white" onClick={handleSearch}>
