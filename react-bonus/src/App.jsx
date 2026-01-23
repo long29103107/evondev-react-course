ce8c1a9d56faa9a33e83c0a8223d3a3420928a69
blogic-hoanglongnguyen <hoanglongnguyen@blogiclab.com>
2026-01-23 09:06:40 +0700

    Add react bonus project

---
+++ react-bonus/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen text-primary">
+      <h1 className="text-4xl font-bold text-red-500">Hello World</h1>
+    </div>
+  )
+}
+
+export default App;
